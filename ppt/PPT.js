2beabf56be186932068589e2c377b041e9e60ae8
Ceditvodu <ceditvodu@gmail.com>
2016-10-30 12:28:14 +0200

    Worked version

--- ppt/PPT.js
+++ ppt/PPT.js
@@ -1,18 +1,24 @@
 'use strict'
 /**
   * @name PPT
-  * @version 1.0.0
+  * @version 1.0.1
   * @author Ivan Kaduk
   * @copyright Ivan Kaduk 2016.
   * @license MIT
-  * @class
-  * @namespace PPT
-  * @constructs
-  * @classdesc This is tool wich can help to create pixel-perfect design
-  * @example var spoiler = new Spoiler('spoiler','opened', 1);
-  * @param {String} options - Name of json file with options.
-  * @param {Object} options - Settings for a project.
-  * @param {String} options.status - Tell us if tool enabled/disables.
+  * @desc It is tool for pixel perfect mark up! 
+  * @example 
+		  ppt({
+				enabled:true,
+				position:"right top",
+				items: {
+					'1201x9999': '1200px.jpg',
+					'960x1200': '960px.jpg',
+					'0x960': '320px.jpg'
+				}
+			});
+  * @param {Boolean} enabled - Status trigger.
+  * @param {String} position - Position of toolbox interface on document.
+  * @param {Object} items - List of items in next structer {'min_resolutionxmax_resolution':'file adress'}.
   */
 
 {
@@ -87,7 +93,7 @@
 				var stencil = new Stencil;
 				stencil.dataset.max_res = Math.max.apply(null, key.split('x'));
 				stencil.dataset.min_res = Math.min.apply(null, key.split('x'));
-				stencil.dataset.url = items[key]
+				stencil.dataset.url = items[key];
 				return stencil;
 			});
 
@@ -129,7 +135,11 @@
 						left:0px;
 						position:absolute;
 						top:0px;
+						bottom: 0px;
 						width:100%;
+				    pointer-events: none;
+				    cursor: move !important;
+				    z-index: 2147483646 !important;
 					}
 					stencil-item{
 						width:100%;
@@ -139,34 +149,61 @@
 						position: absolute;
 						background-repeat: no-repeat;
 						background-position: top center;
-
+						opacity:.5;
+					}
+					stencil-item img{
+						margin: 0 auto;
+						display: block;
 					}
 			`;
 		body.append(style);
 	}
 }
 
+/**
+  * @name StencilItem
+  * @author Ivan Kaduk
+  * @copyright Ivan Kaduk 2016.
+  * @license MIT
+  * @class
+  * @constructs
+  * @classdesc It is web component that realise one entity that hold .jpg image 
+  * @example 
+			var stencil = new Stencil;
+			stencil.dataset.max_res = 1200;
+			stencil.dataset.min_res = 960;
+			stencil.dataset.url = 'main-1200.jpg';
+  * @param {Number} dataset.max_res - Maximum resolution breackpoint.
+  * @param {Number} dataset.min_res - Minimum resolution breackpoint.
+  * @param {String} dataset.url - Url to image.
+  */
 class StencilItem extends HTMLDivElement{
 	constructor(){
 		super();
 	}
 
 	createdCallback(){
-		console.log(this);
-
-		// this.hidden = true;
+		this.item_id = Math.round(Math.random()*10000);
+		this.className = "stencils_item stencils_item__"+this.item_id;
 	}
 
 	attachedCallback(){
-		this.style.backgroundImage = 'url('+this.dataset.url+')';
 
-
+		this.innerHTML = `
+			<img src="`+this.dataset.url+`">
+			<style>
+				.stencils_item__`+this.item_id+`{
+					display: none;
+				}
+				@media screen and (min-width:`+this.dataset.min_res+`px) and (max-width:`+this.dataset.max_res+`px){
+					.stencils_item__`+this.item_id+`{
+						display: block;
+					}
+				}
+			</style>
+		`;
 	}
-
- 
-
 }
-
 var Stencil = document.registerElement("stencil-item", StencilItem);
 
 
@@ -175,8 +212,8 @@ ppt({
 	position:"right top",
 	items: {
 		'1201x9999': '1200px.jpg',
-		'321x1200': '960px.jpg',
-		'0x320': '320px.jpg'
+		'960x1200': '960px.jpg',
+		'0x960': '320px.jpg'
 	}
 });
 
